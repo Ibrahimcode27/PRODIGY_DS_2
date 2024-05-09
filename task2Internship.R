0a0fadd735c2860201675dbc50359d0e71f1439a
Ibrahim Mulla <ibrahimmulla2727@gmail.com>
2024-05-10 01:41:28 +0530

    added files and results

---
+++ task2Internship.R
@@ -0,0 +1,14 @@
+# Load necessary library
+library(titanic)
+
+# Load Titanic dataset
+data("titanic_train")
+
+# Check structure of the dataset
+str(titanic_train)
+
+# View the first few rows
+head(titanic_train)
+
+# Summary statistics
+summary(titanic_train)
